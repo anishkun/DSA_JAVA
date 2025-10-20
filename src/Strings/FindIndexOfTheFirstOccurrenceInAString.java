955da04ae81198c8aa9780a3cba8047d94d04227
Anish K <anish03anish@gmail.com>
2025-10-20 17:38:39 +0530

    Added Find index of the first occurrence in a String question

---
+++ src/Strings/FindIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class FindIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
