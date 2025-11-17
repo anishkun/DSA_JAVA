031b54387c5fbffe61b9a59ff12f1cbd496bf1c5
anishkun <anish03anish@gmail.com>
2025-11-17 20:20:30 +0530

    added majority element question

---
+++ src/Arrays/MajorityElement.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int maj = nums[0];
+        int count = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] != maj){
+                count--;
+                if(count==0){
+                    maj=nums[i];
+                    count=1;
+                }
+            }else{
+                count++;
+            }
+        }
+
+        return maj;
+    }
+}
