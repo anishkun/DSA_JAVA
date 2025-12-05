79a2f0b9d358aee6067385058c4ead0cfe95ea8d
anishkun <anish03anish@gmail.com>
2025-12-05 19:35:12 +0530

    added lca of bst question

---
+++ src/Trees/LowestCommonAcestorInBinarySearchTree.java
@@ -0,0 +1,13 @@
+package Trees;
+
+public class LowestCommonAcestorInBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
